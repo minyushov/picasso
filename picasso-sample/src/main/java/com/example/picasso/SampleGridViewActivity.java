ee119ddaa9b70039a3174ea8d8b7aa85f6ab4cca
Semyon Minyushov <minyushov@gmail.com>
2019-06-26 00:14:28 +0400

    Clean up dependencies

--- picasso-sample/src/main/java/com/example/picasso/SampleGridViewActivity.java
+++ picasso-sample/src/main/java/com/example/picasso/SampleGridViewActivity.java
@@ -9,7 +9,7 @@ public class SampleGridViewActivity extends PicassoSampleActivity {
     setContentView(R.layout.sample_gridview_activity);
 
     GridView gv = findViewById(R.id.grid_view);
-    gv.setAdapter(new SampleGridViewAdapter(this));
+    gv.setAdapter(new SampleGridViewAdapter(this, false));
     gv.setOnScrollListener(new SampleScrollListener(this));
   }
 }
